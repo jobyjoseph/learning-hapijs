bcb407f1f84739f9223996db8cd155057dc64606
Joby Joseph <reachjoby@gmail.com>
2017-01-24 23:32:50 +0530

    basic hapijs app

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({port: 4000, host: 'localhost'});
+
+server.route({
+	method: 'GET',
+	path: '/',
+	handler: function (request, reply) {
+		reply('Hello World');
+	}
+});
+
+server.route({
+	method: 'GET',
+	path: '/{name}',
+	handler: function(request, reply) {
+		reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+	}
+});
+
+server.start((err) => {
+	if (err) {
+		throw err;
+	}
+	console.log('Server running ...');
+});
